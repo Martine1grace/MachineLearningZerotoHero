0b48588512bf2f47e006dbfc8ea91948a9485609
Martine1grace <martinegrace16@gmail.com>
2022-02-15 12:12:18 +0200

    firstassignment

---
+++ letsupgrade (1).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# In[11]:
+
+
+##assignment1
+c=np.random.randint(10,50,(6,5))
+c
+
+
+# In[30]:
+
+
+c[3:5,2:]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
